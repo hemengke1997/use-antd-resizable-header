c40a02298384d2d26c25afa64088222a0520bbc7
hemengke <23536175@qq.com>
2023-11-27 01:41:33 +0800

    fix: playground #67 type

--- playground/src/67/index.tsx
+++ playground/src/67/index.tsx
@@ -1,22 +1,22 @@
-import ProTable, { type ProColumnType, type ProColumns } from '@ant-design/pro-table'
+import ProTable, { type ProColumns } from '@ant-design/pro-table'
 import Table, { type ColumnsType } from 'antd/es/table'
 import { useState } from 'react'
 import { useAntdResizableHeader } from 'use-antd-resizable-header'
 
 export default function () {
   const [proCols, _setProCols] = useState<ProColumns<any>[]>([])
-  const {} = useAntdResizableHeader<ProColumnType>({
+  const { resizableColumns: proResizableColumns } = useAntdResizableHeader({
     columns: proCols,
   })
 
-  const [cols, _setCols] = useState<ColumnsType>([{ title: 'Name', dataIndex: 'name' }])
-  const { resizableColumns } = useAntdResizableHeader<ColumnsType[number]>({
+  const [cols, _setCols] = useState<ColumnsType<any>>([{ title: 'Name', dataIndex: 'name' }])
+  const { resizableColumns } = useAntdResizableHeader({
     columns: cols,
   })
 
   return (
     <>
-      <ProTable columns={proCols}></ProTable>
+      <ProTable columns={proResizableColumns}></ProTable>
       <Table columns={resizableColumns} />
     </>
   )
