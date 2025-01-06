33bfe4f72d63966920ceb64fa9a7c6c002dbfb8c
chenliandong <chenliandong@chipcoo.com>
2025-01-06 20:04:19 +0800

    fix: #107

--- src/hooks/useGetDataIndexColumns.ts
+++ src/hooks/useGetDataIndexColumns.ts
@@ -11,8 +11,16 @@ export function getUniqueId(s: string) {
 }
 
 function getColumns<T extends ResizableColumnType>(list: T[] | undefined): any {
-  const trulyColumns = list?.filter((item) => !isEmpty(item))
+  // Table.SELECTION_COLUMN 和 Table.EXPANDABLE_COLUMN 为 {}，会被过滤掉，需要保留
+  // 因 Antd 侧只想用 Table.SELECTION_COLUMN 和 Table.EXPANDABLE_COLUMN 的引用值是否相等来判断是否为内置列
+  // 而当组件和业务的 Table 不是同一个引用时，只通过引用值判断会导致内置列被过滤掉
+  // 故采用 JSON.stringify 判断是否为内置列
+  const isReservedColumn = (item) => JSON.stringify(item) === '{}'
+  const trulyColumns = list?.filter((item) => !isEmpty(item) || isReservedColumn(item))
   const c = trulyColumns?.map((col, index) => {
+    if (isReservedColumn(col)) {
+      return col
+    }
     return {
       ...col,
       children: col?.children?.length ? getColumns(col.children) : undefined,
